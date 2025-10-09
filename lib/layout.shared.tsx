6865a970fead21ef08a65ee7bae763fd70a06e9e
markshawn2020 <shawninjuly@gmail.com>
2025-10-09 17:18:49 +0800

    fix: layout no navs

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -4,12 +4,26 @@ export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: 'ReelVan',
+      url: '/',
     },
     links: [
+      {
+        text: 'Pricing',
+        url: '/pricing',
+      },
       {
         text: 'Blog',
         url: '/blog',
         active: 'nested-url',
+      },
+      {
+        text: 'Docs',
+        url: '/docs',
+        active: 'nested-url',
+      },
+      {
+        text: 'About',
+        url: '/about',
       },
     ],
   }
