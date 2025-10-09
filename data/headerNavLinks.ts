078d0bf5f71e3dbb3ea04c50e654deefcf8d8e55
markshawn2020 <shawninjuly@gmail.com>
2025-10-09 18:12:33 +0800

    feat: update about-me

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/pricing', title: 'Pricing' },
   { href: '/blog', title: 'Blog' },
   { href: '/docs', title: 'Docs' },
-  { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
