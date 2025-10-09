270fa9f7d210fb0b5ea45bb540e566e99d7f2323
markshawn2020 <shawninjuly@gmail.com>
2025-10-09 17:29:13 +0800

    feat: better docs

--- .source/index.ts
+++ .source/index.ts
@@ -1,10 +1,10 @@
 // @ts-nocheck -- skip type checking
-import * as docs_5 from '../content/docs/watermark-removal.mdx?collection=docs&hash=1759999677267'
-import * as docs_4 from '../content/docs/quality-enhancement.mdx?collection=docs&hash=1759999677267'
-import * as docs_3 from '../content/docs/pricing.mdx?collection=docs&hash=1759999677267'
-import * as docs_2 from '../content/docs/index.mdx?collection=docs&hash=1759999677267'
-import * as docs_1 from '../content/docs/getting-started.mdx?collection=docs&hash=1759999677267'
-import * as docs_0 from '../content/docs/aspect-ratio.mdx?collection=docs&hash=1759999677267'
+import * as docs_5 from '../content/docs/features/watermark-removal.mdx?collection=docs&hash=1759999677267'
+import * as docs_4 from '../content/docs/features/quality-enhancement.mdx?collection=docs&hash=1759999677267'
+import * as docs_3 from '../content/docs/features/aspect-ratio.mdx?collection=docs&hash=1759999677267'
+import * as docs_2 from '../content/docs/resources/pricing.mdx?collection=docs&hash=1759999677267'
+import * as docs_1 from '../content/docs/index.mdx?collection=docs&hash=1759999677267'
+import * as docs_0 from '../content/docs/getting-started.mdx?collection=docs&hash=1759999677267'
 import * as blog_3 from '../content/blog/remove-veo-watermark.mdx?collection=blog&hash=1759999677267'
 import * as blog_2 from '../content/blog/remove-sora-watermark.mdx?collection=blog&hash=1759999677267'
 import * as blog_1 from '../content/blog/remove-kling-watermark.mdx?collection=blog&hash=1759999677267'
@@ -55,19 +55,34 @@ export const blog = _runtime.docs<typeof _source.blog>(
 )
 export const docs = _runtime.docs<typeof _source.docs>(
   [
-    { info: { path: 'aspect-ratio.mdx', fullPath: 'content/docs/aspect-ratio.mdx' }, data: docs_0 },
     {
       info: { path: 'getting-started.mdx', fullPath: 'content/docs/getting-started.mdx' },
-      data: docs_1,
+      data: docs_0,
     },
-    { info: { path: 'index.mdx', fullPath: 'content/docs/index.mdx' }, data: docs_2 },
-    { info: { path: 'pricing.mdx', fullPath: 'content/docs/pricing.mdx' }, data: docs_3 },
+    { info: { path: 'index.mdx', fullPath: 'content/docs/index.mdx' }, data: docs_1 },
     {
-      info: { path: 'quality-enhancement.mdx', fullPath: 'content/docs/quality-enhancement.mdx' },
+      info: { path: 'resources/pricing.mdx', fullPath: 'content/docs/resources/pricing.mdx' },
+      data: docs_2,
+    },
+    {
+      info: {
+        path: 'features/aspect-ratio.mdx',
+        fullPath: 'content/docs/features/aspect-ratio.mdx',
+      },
+      data: docs_3,
+    },
+    {
+      info: {
+        path: 'features/quality-enhancement.mdx',
+        fullPath: 'content/docs/features/quality-enhancement.mdx',
+      },
       data: docs_4,
     },
     {
-      info: { path: 'watermark-removal.mdx', fullPath: 'content/docs/watermark-removal.mdx' },
+      info: {
+        path: 'features/watermark-removal.mdx',
+        fullPath: 'content/docs/features/watermark-removal.mdx',
+      },
       data: docs_5,
     },
   ],
@@ -76,16 +91,18 @@ export const docs = _runtime.docs<typeof _source.docs>(
       info: { path: 'meta.json', fullPath: 'content/docs/meta.json' },
       data: {
         title: 'Documentation',
-        pages: [
-          'index',
-          'getting-started',
-          '---Features',
-          'watermark-removal',
-          'quality-enhancement',
-          'aspect-ratio',
-          '---Resources',
-          'pricing',
-        ],
+        pages: ['index', 'getting-started', 'features', 'resources'],
+      },
+    },
+    {
+      info: { path: 'resources/meta.json', fullPath: 'content/docs/resources/meta.json' },
+      data: { title: 'Resources', pages: ['pricing'] },
+    },
+    {
+      info: { path: 'features/meta.json', fullPath: 'content/docs/features/meta.json' },
+      data: {
+        title: 'Features',
+        pages: ['watermark-removal', 'quality-enhancement', 'aspect-ratio'],
       },
     },
   ]
